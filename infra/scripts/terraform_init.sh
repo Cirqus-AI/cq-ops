f4ac83d2dc3a13e9556d9b951f464d805859e714
Alexis Manuel <alexis.manuelpro@gmail.com>
2020-09-13 14:29:08 +0200

    :whale: Install terraform in Dockerfile

--- infra/scripts/terraform_init.sh
+++ infra/scripts/terraform_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install unzip
+
 wget https://releases.hashicorp.com/terraform/0.13.1/terraform_0.13.1_linux_amd64.zip
 unzip terraform_0.13.1_linux_amd64.zip
 mv terraform /opt/terraform
